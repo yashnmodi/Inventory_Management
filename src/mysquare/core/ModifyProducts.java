59e805c6b899407e8676b095ca4564d518b15c2c
Yash Modi <myn.yashnm@gmail.com>
2019-11-10 16:57:24 +0530

    required external files

--- src/mysquare/core/ModifyProducts.java
+++ src/mysquare/core/ModifyProducts.java
@@ -11,12 +11,12 @@ public class ModifyProducts {
         JTextField product = new JTextField(40);
         product.setToolTipText("Enter unique product");
         product.setBounds(15, 25, 250, 20);
-        JTextField weight = new JTextField(40);
-        weight.setToolTipText("Enter unique weight");
-        weight.setBounds(15, 60, 250, 20);
         JTextField colour = new JTextField(40);
         colour.setToolTipText("Enter unique colour");
-        colour.setBounds(15, 95, 250, 20);
+        colour.setBounds(15, 60, 250, 20);
+        JTextField weight = new JTextField(40);
+        weight.setToolTipText("Enter unique weight");
+        weight.setBounds(15, 95, 250, 20);
         JButton addProduct = new JButton("Add Product");
         addProduct.setBounds(280, 25, 120, 20);
         JButton addColour = new JButton("Add Colour");
@@ -31,73 +31,61 @@ public class ModifyProducts {
         rmvWeight.setBounds(410, 95, 120, 20);
         panel.setLayout(null);
         panel.add(product);
+        panel.add(colour);
+        panel.add(weight);
         panel.add(addProduct);
         panel.add(rmvProduct);
-        panel.add(colour);
         panel.add(addColour);
         panel.add(rmvColour);
-        panel.add(weight);
         panel.add(addWeight);
         panel.add(rmvWeight);
 
-        addProduct.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                try {
-                    Db.addItem("product_list", product.getText());
-                } catch (Exception err) {
-                    JOptionPane.showConfirmDialog(IMStart.frame, "Product already added.\nERROR:"+err.getMessage(),"WARNING",JOptionPane.WARNING_MESSAGE );
-                }
+        addProduct.addActionListener(e -> {
+            try {
+                Db.addItem("product_list", product.getText());
+                product.setText("");
+            } catch (Exception err) {
+                JOptionPane.showConfirmDialog(IMStart.frame, "Product already added.\nERROR:"+err.getMessage(),"WARNING",JOptionPane.WARNING_MESSAGE );
             }
         });
-        addColour.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                try {
-                    Db.addItem("colour_list", colour.getText());
-                } catch (Exception err) {
-                    JOptionPane.showConfirmDialog(IMStart.frame, "Colour already added.\nERROR:"+err.getMessage(),"WARNING",JOptionPane.WARNING_MESSAGE );
-                }
+        addColour.addActionListener(e -> {
+            try {
+                Db.addItem("colour_list", colour.getText());
+                colour.setText("");
+            } catch (Exception err) {
+                JOptionPane.showConfirmDialog(IMStart.frame, "Colour already added.\nERROR:"+err.getMessage(),"WARNING",JOptionPane.WARNING_MESSAGE );
             }
         });
-        addWeight.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                try {
-                    Db.addItem("weight_list", weight.getText());
-                } catch (Exception err) {
-                    JOptionPane.showConfirmDialog(IMStart.frame, "Weight already added.\nERROR:"+err.getMessage(),"WARNING",JOptionPane.WARNING_MESSAGE );
-                }
+        addWeight.addActionListener(e -> {
+            try {
+                Db.addItem("weight_list", weight.getText());
+                weight.setText("");
+            } catch (Exception err) {
+                JOptionPane.showConfirmDialog(IMStart.frame, "Weight already added.\nERROR:"+err.getMessage(),"WARNING",JOptionPane.WARNING_MESSAGE );
             }
         });
-        rmvProduct.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                try {
-                    Db.removeItem("product_list","pname", product.getText());
-                } catch (Exception err) {
-                    JOptionPane.showConfirmDialog(IMStart.frame, "Product not found.\nERROR:"+err.getMessage(),"WARNING",JOptionPane.WARNING_MESSAGE );
-                }
+        rmvProduct.addActionListener(e -> {
+            try {
+                Db.removeItem("product_list","pname", product.getText());
+                product.setText("");
+            } catch (Exception err) {
+                JOptionPane.showConfirmDialog(IMStart.frame, "Product not found.\nERROR:"+err.getMessage(),"WARNING",JOptionPane.WARNING_MESSAGE );
             }
         });
-        rmvColour.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                try {
-                    Db.removeItem("colour_list", "pclr", colour.getText());
-                } catch (Exception err) {
-                    JOptionPane.showConfirmDialog(IMStart.frame, "Colour not found.\nERROR:"+err.getMessage(),"WARNING",JOptionPane.WARNING_MESSAGE );
-                }
+        rmvColour.addActionListener(e -> {
+            try {
+                Db.removeItem("colour_list", "pclr", colour.getText());
+                colour.setText("");
+            } catch (Exception err) {
+                JOptionPane.showConfirmDialog(IMStart.frame, "Colour not found.\nERROR:"+err.getMessage(),"WARNING",JOptionPane.WARNING_MESSAGE );
             }
         });
-        rmvWeight.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                try {
-                    Db.removeItem("weight_list", "pwt", weight.getText());
-                } catch (Exception err) {
-                    JOptionPane.showConfirmDialog(IMStart.frame, "Weight not found.\nERROR:"+err.getMessage(),"WARNING",JOptionPane.WARNING_MESSAGE );
-                }
+        rmvWeight.addActionListener(e -> {
+            try {
+                Db.removeItem("weight_list", "pwt", weight.getText());
+                weight.setText("");
+            } catch (Exception err) {
+                JOptionPane.showConfirmDialog(IMStart.frame, "Weight not found.\nERROR:"+err.getMessage(),"WARNING",JOptionPane.WARNING_MESSAGE );
             }
         });
 
